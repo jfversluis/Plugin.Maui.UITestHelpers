4498a2d7dff2c5b571fd32d68c5034acc21a33f9
Gerald Versluis <gerald@verslu.is>
2024-03-25 09:13:36 +0100

    Update MainPage.xaml.cs

--- samples/Plugin.Maui.Feature.Sample/MainPage.xaml.cs
+++ samples/Plugin.Maui.Feature.Sample/MainPage.xaml.cs
@@ -4,12 +4,8 @@ namespace Plugin.Maui.Feature.Sample;
 
 public partial class MainPage : ContentPage
 {
-	readonly IFeature feature;
-
 	public MainPage(IFeature feature)
 	{
 		InitializeComponent();
-		
-		this.feature = feature;
 	}
 }
