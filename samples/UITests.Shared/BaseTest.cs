a0b5f38fb7bceb92c950d800c7935ad087571574
Gerald Versluis <gerald.versluis@microsoft.com>
2024-03-28 12:15:18 +0100

    Fix iOS

--- samples/UITests.Shared/BaseTest.cs
+++ samples/UITests.Shared/BaseTest.cs
@@ -24,10 +24,18 @@ public abstract class BaseTest : UITestBase
 
         if (_testDevice == TestDevice.iOS)
         {
-            // Note: this must match to the Simulator created in the GitHub Action
-            // Both the device name and iOS version must be here in order for Appium to find it correctly
-            config.SetProperty("DeviceName", "UITestSim");
-            config.SetProperty("PlatformVersion", "17.2");
+            // Note: this is passed down from the GitHub Action. If nothing is set, fall back to a default value below
+            if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("SIMID"))
+                && !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("SIMNAME")))
+            {
+                config.SetProperty("DeviceName", Environment.GetEnvironmentVariable("SIMNAME"));
+                config.SetProperty("udid", Environment.GetEnvironmentVariable("SIMID"));
+            }
+            else
+            {
+                config.SetProperty("DeviceName", "iPhone 15 Pro");
+                config.SetProperty("PlatformVersion", "17.2");
+            }
         }
 
         return config;
