446868b1fce54f35371f8080fbbf4c0f27a39ed1
Gerald Versluis <gerald@verslu.is>
2024-03-31 19:50:37 +0200

    Try iOS 16.4

--- samples/UITests.Shared/BaseTest.cs
+++ samples/UITests.Shared/BaseTest.cs
@@ -31,7 +31,7 @@ public abstract class BaseTest : UITestBase
             {
                 config.SetProperty("DeviceName", Environment.GetEnvironmentVariable("SIMNAME"));
                 config.SetProperty("udid", Environment.GetEnvironmentVariable("SIMID"));
-                config.SetProperty("PlatformVersion", "17.2");
+                config.SetProperty("PlatformVersion", "16.4");
             }
             else
             {
