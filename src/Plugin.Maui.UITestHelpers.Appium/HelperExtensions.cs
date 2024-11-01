0596b0b7cb50eacf315fb0281e25d5debd7956a2
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-11-01 21:49:44 +0200

    Added more method to Helper Extensions

--- src/Plugin.Maui.UITestHelpers.Appium/HelperExtensions.cs
+++ src/Plugin.Maui.UITestHelpers.Appium/HelperExtensions.cs
@@ -907,6 +907,34 @@ namespace Plugin.Maui.UITestHelpers.Appium
         }
 
         /// <summary>
+        /// Sets light device's theme
+        /// </summary>
+        /// <param name="app">Represents the main gateway to interact with an app.</param>
+        public static void SetLightTheme(this IApp app)
+        {
+            if (app is not AppiumAndroidApp && app is not AppiumIOSApp)
+            {
+                throw new InvalidOperationException($"SetLightTheme is not supported");
+            }
+
+            app.CommandExecutor.Execute("setLightTheme", ImmutableDictionary<string, object>.Empty);
+        }
+
+        /// <summary>
+        /// Sets dark device's theme
+        /// </summary>
+        /// <param name="app">Represents the main gateway to interact with an app.</param>
+        public static void SetDarkTheme(this IApp app)
+        {
+            if (app is not AppiumAndroidApp && app is not AppiumIOSApp)
+            {
+                throw new InvalidOperationException($"SetDarkTheme is not supported");
+            }
+
+            app.CommandExecutor.Execute("setDarkTheme", ImmutableDictionary<string, object>.Empty);
+        }
+
+        /// <summary>
         /// Return the AppId of the running app. This is used inside any appium command that want the app id
         /// </summary>
         /// <param name="app">Represents the main gateway to interact with an app.</param>
